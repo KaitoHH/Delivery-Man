0802a7a63b9f10a39f6569518afbce53881324d0
KaitoHH <hh.kaito@gmail.com>
2018-12-14 13:34:23 +0800

    add auto login

--- Mini/pages/login/login.js
+++ Mini/pages/login/login.js
@@ -3,32 +3,50 @@ Page({
 	data: {
 		canIUse: wx.canIUse('button.open-type.getUserInfo')
 	},
-	bindGetUserInfo: function (e) {
-		console.log(e);
-		if (e.detail.userInfo) {
-			wx.showLoading({
-				title: '登录中',
-			})
-			console.log(e.detail.userInfo)
-			this.setData({
-				userInfo: e.detail.userInfo
-			})
-			app.globalData.userInfo = this.data.userInfo
-			app.user.updateUserNickName(app.globalData.userId, app.globalData.userInfo.nickName)
+	login: function (userInfo) {
+		wx.showLoading({
+			title: '登录中',
+		})
+		console.log(userInfo)
+		this.setData({
+			userInfo: userInfo
+		})
+		app.globalData.userInfo = this.data.userInfo
+		app.user.updateUserNickName(app.globalData.userId, app.globalData.userInfo.nickName)
 			.then(res => {
 				console.log(res)
 			}).catch(e => {
 				console.log(e)
 			})
-			console.log('switch page');
-			setTimeout(() => {
-				wx.switchTab({
-					url: '/pages/index/index',
-					fail: function (res) {
-						console.log(res)
-					}
-				});
-			}, 0);
+		console.log('switch page');
+		setTimeout(() => {
+			wx.switchTab({
+				url: '/pages/index/index',
+				fail: function (res) {
+					console.log(res)
+				}
+			});
+		}, 0);
+	},
+	onLoad() {
+		let that = this
+		wx.getSetting({
+			success(res) {
+				if (res.authSetting['scope.userInfo']) {
+					wx.getUserInfo({
+						success(res) {
+							console.log(res.userInfo)
+							that.login(res.userInfo)
+						}
+					})
+				}
+			}
+		})
+	},
+	bindGetUserInfo: function (e) {
+		console.log(e);
+		if (e.detail.userInfo) {
+			this.login(e.detail.userInfo)
 		} else {
 			wx.showToast({ title: '拒绝怎么给你用？', icon: 'none' })
 		}
