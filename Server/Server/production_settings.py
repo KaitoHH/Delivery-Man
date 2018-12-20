4c11415a46881d9662080b81a3f9cb62a05f5293
KaitoHH <hh.kaito@gmail.com>
2018-12-20 16:10:13 +0800

    bug fix

--- Server/Server/production_settings.py
+++ Server/Server/production_settings.py
@@ -40,6 +40,6 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/data/db.sqlite3',
     }
 }
